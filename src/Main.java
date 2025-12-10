dd8aa56a90d00e3b8e4e63df7a6c34a2cd4d3e3a
Yangerlol <yanis4rtuna@gmail.com>
2025-12-10 18:59:12 +0300

    Add miles calculation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        int currentAccount = 100; // У клиента на счету
+        int refillAmount = 1100;  // Клиент пополнил на (меняйте это значение для тестов)
+
+        int bonus;
+
+        // Проверяем, больше ли пополнение 1000 рублей
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100; // 1 рубль за каждые 100 рублей
+        } else {
+            bonus = 0; // Бонусов нет
+        }
+
+        int finalAccount = currentAccount + refillAmount + bonus;
+
+        System.out.println("Итоговый счет: " + finalAccount);
+        System.out.println("Бонусные рубли: " + bonus);
+    }
 }
